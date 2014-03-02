b6179d0702e9f70f21035e5365ab3618ad2dd65c
sreshetnyak <reshetniak.sergey@gmail.com>
2014-03-02 20:58:32 +0200

    Initial commit from local git

---
+++ Gestures/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  Gestures
+//
+//  Created by sergey on 3/2/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
